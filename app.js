52dba4a8c722fa939d416d8a13cb4f6877a6cc6c
Wil Ledbetter <wdledbetter@gmail.com>
2021-06-20 21:33:18 -0500

    Completed Exercises 1,2,3

--- app.js
+++ app.js
@@ -4,7 +4,23 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Wil";
+let lastName = "Ledbetter";
+
+var age = "26";
+let isDone;
 
 // Exercise 2
+let fullName = firstName + " " + lastName;
+let templateFullName = `${firstName} ${lastName}`
+console.log("fullName: ", fullName)
+console.log("templatefullName: ", templateFullName)
 
 // Exercise 3
+let myStory;
+let city = "Birmingham, AL";
+let pasttime = "producing music/audio engineering, painting/multimedia art, and outdoors exploration/hiking";
+
+myStory = `My name is ${fullName}. I live in ${city}. I like ${pasttime}. `;
+
+console.log(myStory);
